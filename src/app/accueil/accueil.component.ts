7929a8c67be61f06cfa0a42f3ff66fc1543d96c5
SaharBenNejma <mokcheha.mohamed@gmail.com>
2024-07-05 03:44:09 +0100

    commit

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit } from '@angular/core';
+declare const $: any; // Déclaration de jQuery pour Angular
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent {
+export class AccueilComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    // Initialisation du carousel Bootstrap
+    $(document).ready(() => {
+      $('.carousel').carousel();
+    });
+  }
 }
